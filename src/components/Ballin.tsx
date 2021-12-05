44f47a8bff2d55a68d08d6322e71fee657d7d193
bluetayden <68973162+bluetayden@users.noreply.github.com>
2021-12-05 13:50:30 +1300

    Added image text

--- src/components/Ballin.tsx
+++ src/components/Ballin.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useWindowSize } from 'react-use';
 import Confetti from 'react-confetti'
 
@@ -8,16 +8,46 @@ import '../styles/Ballin.css'
 
 const Ballin = () => {
     const [showConfetti, setShowConfetti] = useState(false);
+    const [ballinCount, setBallinCount] = useState(0);
+    const ballinTextTimerRef: { current: NodeJS.Timeout | null } = useRef(null);
+    const [showBallinText, setShowBallinText] = useState(false);
+
     const { width, height } = useWindowSize()
+
+    /** Shows confetti, incs click count, shows click count text */
+    function handleBallinClick() {
+        setShowConfetti(true);
+        setBallinCount(ballinCount + 1);
+
+        handleShowBallinText();
+    }
+
+    /** Starts a timer that keeps resetting till the ballin image isn't clicked for 4 seconds */
+    function handleShowBallinText() {
+        setShowBallinText(true);
+
+        if (ballinTextTimerRef.current === null) {
+            ballinTextTimerRef.current = setTimeout(() => {setShowBallinText(false); ballinTextTimerRef.current = null}, 4000)
+        } else {
+            clearTimeout(ballinTextTimerRef.current);
+            ballinTextTimerRef.current = setTimeout(() => {setShowBallinText(false); ballinTextTimerRef.current = null}, 4000)
+        }
+    }
 
     return (
         <div className="ballin-container">
+            {showBallinText &&
+                <div className="ballin-text">
+                    you've balled {ballinCount} times
+                </div>}
+
             <img src={BallinDotPng} className="ballin-img" alt="ballin"
-                onClick={() => setShowConfetti(true)} />
+                onClick={() => handleBallinClick()} />
 
             {showConfetti &&
                 <Confetti width={width} height={height}
-                    numberOfPieces={200} recycle={false} onConfettiComplete={() => setShowConfetti(false)} />}
+                    numberOfPieces={200} recycle={false} initialVelocityY={{ min: 5, max: 5 }}
+                    onConfettiComplete={() => setShowConfetti(false)} />}
         </div>
     )
 }
